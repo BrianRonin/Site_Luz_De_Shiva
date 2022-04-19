2a3ec33810e5a516703e2ff1bcc53a6453f9d1bd
BrianRonin <55673280+BrianRonin@users.noreply.github.com>
2022-04-19 20:22:25 -0300

    Home criado

---
+++ script.js
@@ -0,0 +1,12 @@
+// $(function () {
+//   $(window).resize(function () {
+//     if ($(window).width() < 596) {
+//       $(".container-c").addClass("d-none");
+
+//       console.log("sim");
+//     } else {
+//       console.log("nao");
+//       $(".container-c").removeClass("d-none");
+//     }
+//   });
+// });
